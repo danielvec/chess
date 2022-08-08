01880379b3dec4f39664c941067cdd7b114deaec
DanielVec <vechioladaniel@gmail.com>
2022-08-07 22:34:22 -0700

    add additional test for color_board method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,6 +11,13 @@ describe Board do
       board_color.color_board
     end
 
+    it 'returns a blue first square' do
+      blue = "\e[0;39;44m   \e[0m"
+      board_color.color_board
+      first_square = board_color.board[1][1]
+      expect(first_square).to eq(blue)
+    end
+
     it 'sends on_white to fifth row, fourth column' do
       middle_square = board_color.board[5][4]
       expect(middle_square).to receive(:on_white)

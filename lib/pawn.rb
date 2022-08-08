84c0ed57dbc4b189c634e7e8a74222e195aedec9
DanielVec <vechioladaniel@gmail.com>
2022-08-07 20:35:31 -0700

    add to_s method to pawn class

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,9 +1,19 @@
 # represents a pawn chess piece
 class Pawn
+  attr_reader :player
+  
   def initialize(player, row, column)
     @player = player
     @row = row
     @column = column
     @moves = 0
+  end
+
+  def to_s
+    if player == 1
+      " \u265F "
+    elsif player == 2
+      " \u265F ".black
+    end
   end
 end

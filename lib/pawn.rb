8fcbc73ca3a3597028deaf60abe62ece34a9c92c
DanielVec <vechioladaniel@gmail.com>
2022-08-14 21:55:16 -0700

    add move_options and highlight_options methods

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -16,4 +16,22 @@ class Pawn
       " \u265F ".black
     end
   end
+
+  def move_options
+    if player == 1
+      highlight_options(- 1)
+    elsif player == 2
+      highlight_options(1)
+    end
+  end
+
+  def highlight_options(direction)
+    if board.empty_space?(row + direction, column)
+      board.highlight_space(row + direction, column)
+      if moves.zero? && board.empty_space?(row + direction + direction, column)
+        board.highlight_space(row + direction + direction, column)
+      end
+    end
+  end
+
 end
